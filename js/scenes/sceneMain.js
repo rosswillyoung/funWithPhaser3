776685905bba8d9fef46d4c3b42f0d3a572fecf4
rosswillyoung <rosswillyoung@gmail.com>
2020-04-27 15:58:48 -0400

    added logic to add more platforms and to make the first ones 'scroll'

--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -17,16 +17,18 @@ class SceneMain extends Phaser.Scene {
 
     this.platforms = this.physics.add.staticGroup();
     this.platforms.create(game.config.width / 2, game.config.height, 'platform');
+    this.platforms.create(150, 330, 'platform').setScale(.2, .4).refreshBody();
     // this.head.body.setVelocity(10, 0);
 
     this.cursors = this.input.keyboard.createCursorKeys();
     this.physics.add.collider(this.player, this.platforms);
-
+    this.gameStarted = false;
     this.createPlatform();
   }
 
   update() {
     if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.gameStarted = true;
       this.player.setVelocityY(-120);
     }
     if (this.cursors.left.isDown && !this.player.body.touching.down) {
@@ -37,13 +39,31 @@ class SceneMain extends Phaser.Scene {
     if (this.player.body.touching.down){
       this.player.setVelocityX(0);
     }
+
+    if (this.gameStarted) {
+      this.platforms.children.entries.forEach((platform) => {
+        platform.y += .2;
+        platform.refreshBody();
+        if (platform.y > 400) {
+          platform.destroy();
+        }
+      });
+      // console.log(this.platforms);
+    };
   }
 
   createPlatform() {
-    this.platform = this.platforms.create(300, 330, 'platform').setScale(.2, .4).refreshBody();
+    let platformNumber = 10;
+    let yRange = 280;
+    for (let i = 0; i <= platformNumber; i++) {
+      this.platforms.create(Phaser.Math.Between(25, 375), Phaser.Math.Between(yRange, yRange - 30), 'platform').setScale(.2, .4).refreshBody();
+      yRange -= 50;
+    }
 
-    this.platform1 = this.platforms.create(150, 350, 'platform');
-    this.platform1.body.setSize(50, 10);
-    this.platform1.setDisplaySize(50, 10);
+    // this.platform = this.platforms.create(300, 330, 'platform').setScale(.2, .4).refreshBody();
+    //
+    // this.platform1 = this.platforms.create(150, 350, 'platform');
+    // this.platform1.body.setSize(50, 10);
+    // this.platform1.setDisplaySize(50, 10);
   }
 }
