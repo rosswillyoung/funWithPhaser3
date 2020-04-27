9539c0e99f100e9b3ea1d1e1e3ec17d5c75df77a
rosswillyoung <rosswillyoung@gmail.com>
2020-04-27 12:59:00 -0400

    Decided to make a small jumping platform game. Added some cursor keys and the beginning platform

--- js/game.js
+++ js/game.js
@@ -6,7 +6,8 @@ let config = {
     physics: {
       default: 'arcade',
       arcade: {
-        debug: true
+        debug: true,
+        gravity: {y: 100}
       }
     },
     scene: [ SceneMain ]
--- js/scenes/sceneMain.js
+++ js/scenes/sceneMain.js
@@ -5,14 +5,34 @@ class SceneMain extends Phaser.Scene {
 
   preload() {
     this.load.image('body', 'assets/images/body.png');
+    this.load.image('platform', 'assets/images/platform.png');
   }
 
   create() {
     // this.head = this.add.image(150, 100, 'body');
-    let body = this.physics.add.group();
-    this.head = body.create(200, 200, 'body');
-    body.create(300, 200, 'body');
+    this.player = this.physics.add.image(game.config.width / 2, game.config.height - 100, 'body');
+    this.player.setBounce(.2);
+    this.player.setCollideWorldBounds(true);
 
+    this.platforms = this.physics.add.staticGroup();
+    this.platforms.create(game.config.width / 2, game.config.height, 'platform');
     // this.head.body.setVelocity(10, 0);
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, this.platforms);
+  }
+
+  update() {
+    if (this.cursors.up.isDown && this.player.body.touching.down) {
+      this.player.setVelocityY(-120);
+    }
+    if (this.cursors.left.isDown && !this.player.body.touching.down) {
+      this.player.setVelocityX(-100);
+    } else if (this.cursors.right.isDown && !this.player.body.touching.down) {
+      this.player.setVelocityX(100);
+    }
+    if (this.player.body.touching.down){
+      this.player.setVelocityX(0);
+    }
   }
 }
